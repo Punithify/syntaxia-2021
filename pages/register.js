4e3e93411532129be95e20c013e1fd5e5ef71359
Punith <punith@pop-os.localdomain>
2021-05-20 22:35:37 +0530

    Registration closed msg

--- pages/register.js
+++ pages/register.js
@@ -1,11 +1,12 @@
 import React from "react";
 import RegistrationComponent from "../components/RegistrationComponent";
 import Layout from "../components/Layout";
+import { Alert } from "reactstrap";
 
 const register = () => {
   return (
     <Layout title="Syntaxia 2021 | Register">
-      <RegistrationComponent />
+      <Alert>Registeration has been closed for Syntaxia 2021</Alert>
     </Layout>
   );
 };
