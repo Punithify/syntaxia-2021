2e4a74fa28127c0c4f2a9a9ea218bf99a1ab27ee
Punith <punith@pop-os.localdomain>
2022-07-05 14:54:14 +0530

    nprogress addition

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,14 @@ import Footer from "../components/FooterComponent";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/globals.css";
 import "font-awesome/css/font-awesome.min.css";
+import "../styles/nprogress.css";
+import nProgress from "nprogress";
+import Router from "next/router";
+
+Router.events.on("routeChangeStart", nProgress.start);
+Router.events.on("routeChangeError", nProgress.done);
+Router.events.on("routeChangeComplete", nProgress.done);
+
 function MyApp({ Component, pageProps }) {
   return (
     <>
